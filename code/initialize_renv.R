351b2766fa199c2476a479632b55f2b6cf9571fc
Peter Graffy <136639354+petergraffy@users.noreply.github.com>
2025-09-24 12:57:39 -0500

    Add files via upload

---
+++ code/initialize_renv.R
@@ -0,0 +1,22 @@
+# Setup R environment using renv
+# Install renv if not already installed:
+if (!requireNamespace("renv", quietly = TRUE)) install.packages("renv")
+
+# Initialize renv for the project:
+renv::init()
+
+# All required packages (deduped)
+pkgs <- c(
+  "knitr","fst","here","tidyverse","arrow","gtsummary",
+  "dplyr","tidyr","stringr","lubridate","purrr","fuzzyjoin","janitor",
+  "MASS","broom","patchwork","ggplot2","ggeffects","gt",
+  "rlang","data.table","readr","glue","scales",
+  "DiagrammeR","DiagrammeRsvg","rsvg", "tidycensus", "fixest", 
+  "marginaleffects", "pscl", "glmmTMB", "digest", "pROC", 
+  "tibble", "forcats", "grid", 'jsonlite'
+)
+pkgs <- unique(pkgs)
+
+# Install and lock
+renv::install(pkgs)
+renv::snapshot(prompt = FALSE)
